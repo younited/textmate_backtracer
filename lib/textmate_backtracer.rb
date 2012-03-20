8eced16c32d88619362a9d82589465ce7c0f11d2
Johan Buts <johan.buts@trustedarea.com>
2012-03-20 12:07:08 +0100

    Some cleanup to make application and framework trace return html_safe links

--- lib/textmate_backtracer.rb
+++ lib/textmate_backtracer.rb
@@ -14,7 +14,7 @@ if defined?(Rails.backtrace_cleaner)
     end
     alias_method_chain :join, :safety
   end
-  
+
   # activesupport (3.0.0) lib/active_support/whiny_nil.rb:48:in `method_missing'
   # app/controllers/job_queues_controller.rb:5:in `index'
   Rails.backtrace_cleaner.add_filter do |line|
@@ -23,40 +23,40 @@ if defined?(Rails.backtrace_cleaner)
       if spec = Bundler.load.specs.find{|s| s.name == gem_name }
         gem_path = spec.full_gem_path
         file = gem_path + '/' + file_path
-        "<a href='txmt://open?url=file://#{file}&line=#{line_number}'>#{line}</a>".html_safe
+        "<a href='txmt://open?url=file://#{file}&line=#{line_number}'>#{line}</a>"
       else
         line
       end
     elsif match = line.match(/(.*):(\d+):in `.*'/)
       _, app_path, line_number = match.to_a
       file = Rails.root.join(app_path)
-      "<a href='txmt://open?url=file://#{file}&line=#{line_number}'>#{line}</a>".html_safe
+      "<a href='txmt://open?url=file://#{file}&line=#{line_number}'>#{line}</a>"
     else
       line
-    end
+    end.html_safe
   end
-  
+
   # Remove all the silencers, which will *certainly* break anyone else's silencers.
   # Well. Don't use silencers in development mode. ;)
   Rails.backtrace_cleaner.remove_silencers!
-  
+
   # Add back a modified "APP_DIRS_PATTERN" from Rails' railties backtrace_cleaner.rb.
   Rails.backtrace_cleaner.add_silencer { |line| line !~ /^(<.*>)?\/?(app|config|lib|test)/ }
 else
   class Exception
     alias :original_clean_backtrace :clean_backtrace
-  
+
     def add_links_to_backtrace(lines)
       lines.collect do |line|
         expanded = line.gsub '#{RAILS_ROOT}', RAILS_ROOT
         if match = expanded.match(/^(.+):(\d+):in/) or match = expanded.match(/^(.+):(\d+)\s*$/)
           file = File.expand_path(match[1])
           line_number = match[2]
-          html = "<a href='txmt://open?url=file://#{file}&line=#{line_number}'>#{line}</a>"
+          "<a href='txmt://open?url=file://#{file}&line=#{line_number}'>#{line}</a>"
         else
           line
         end
-      end
+      end.html_safe
     end
 
     def clean_backtrace
